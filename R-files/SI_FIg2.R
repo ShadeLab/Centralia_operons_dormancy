60e62130ede8f6d4063416167315f733a6b22b88
Patrick Keanrs <kearnspa@msu.edu>
2017-12-27 14:07:33 -0500

    Update SI_FIg2.R

--- R-files/SI_FIg2.R
+++ R-files/SI_FIg2.R
@@ -4,14 +4,22 @@
 
 #analysis performed in R V 3.4
 #Correlation between PICRUSt and rrnDB in Centralia
-picrust_rrndb <- read.delim("picrust_rrndb.txt")
+picrust_rrndb <- read.delim("C:/Users/patty/Documents/picrust_rrndb.txt")
+picrust_rrndb<-picrust_rrndb[,-4]
+library(reshape2)
+picrust_rrndb_m<-melt(picrust_rrndb)
+library(plyr)
+cent_cor<-ddply(picrust_rrndb_m, c("Type", "variable"), summarise, N= length(value), mean=mean(value), sd=sd(value), se=sd/sqrt(N))
 library(ggplot2)
-ggplot(picrust_rrndb, aes(Weighted_Copy_PC, Weighted_Copy_rrn, colour=Type))+
-  geom_point(aes(cex=1.2))+
+cor_pos<-position_dodge(width=0.3)
+ggplot(cent_cor, aes(Type, mean, colour=variable))+
+  geom_point(aes(cex=1.2), position=cor_pos)+
+  scale_size(guide='none')+
   theme_bw()+
-  ylab("Weighted Mean Copy Number- rrndb")+
-  xlab("Weighted Mean Copy Number- PICRUSt")+
-  scale_size(guide='none')
+  ylab("Weighted Mean Copy Number")+
+  xlab("")+
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se, width=0.2), position=cor_pos)
+  
   
 
 cor.test(picrust_rrndb$Weighted_Copy_PC, picrust_rrndb$Weighted_Copy_rrn, method='spearman')
